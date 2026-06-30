157530481553dc06cbbf3f1d8293cb62a095898b
IcoIvIa <kmyg.jp@gmail.com>
2026-06-30 21:13:07 +0900

    軽微な修正

--- src/Components/SearchNotFound.jsx
+++ src/Components/SearchNotFound.jsx
@@ -2,6 +2,7 @@ function SearchNotFound() {
     return (
         <div id="infoViewText">
             <p>No found.</p>
+            <p>右下のローディングアイコンが表示している場合はデータの取得が不完全な場合があります。データのロードが完了してから再度検索してください。</p>
         </div>
     )
 }
